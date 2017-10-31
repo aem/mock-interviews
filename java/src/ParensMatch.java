da3acfbfb79025824841aa2590e35c2f4e6efcb9
smithnic <nsmith4932@gmail.com>
2017-10-31 11:02:54 -0400

    solved ParensMatch

--- java/src/ParensMatch.java
+++ java/src/ParensMatch.java
@@ -13,6 +13,21 @@ public class ParensMatch {
    * @return whether or not the given string has matching parentheses
    */
   static boolean parensMatch(String str) {
+    int openSquareCount = 0;
+    int openParenCount = 0;
+    for (int i = 0; i < str.length(); i++) {
+        if (str.charAt(i) == '[') openSquareCount++;
+        else if (str.charAt(i) == '(') openParenCount++;
+        else if (str.charAt(i) == ']') {
+            if (openSquareCount == 0) return false;
+            else openSquareCount--;
+        } 
+        else if (str.charAt(i) == ')') {
+            if (openParenCount == 0) return false;
+            else openParenCount--;
+        }       
+    }
+    if (openSquareCount == 0 && openParenCount == 0) return true;
     return false;
   }
 }
