a31e75865455f1dc93aa649921573b880fc57c55
aem <amarkon895@gmail.com>
2017-10-31 11:00:50 -0400

    solved javascript palindrome

--- javascript/palindrome.js
+++ javascript/palindrome.js
@@ -4,7 +4,17 @@ const { assert } = require('./test-utils');
  * @param {string} str  the string to validate
  * @returns {boolean} whether or not the given string is a palindrome
  */
-function isPalindrome(str) {}
+function isPalindrome(str) {
+  let start = 0, end = str.length - 1;
+  for (; start > end; ) {
+    if (str.charAt(start) !== str.charAt(end)) {
+      return false;
+    }
+    start++;
+    end--;
+  }
+  return true;
+}
 
 assert(isPalindrome(''), true, 'empty string validates');
 assert(isPalindrome('adam'), false, '"adam" is not a palindrome');
